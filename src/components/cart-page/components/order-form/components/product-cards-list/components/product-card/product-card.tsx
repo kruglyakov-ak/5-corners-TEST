3f07b92124529246688335ea6cb9680892d8c3c4
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-02-28 23:54:33 +0300

    реализовал логику количества товаров

--- src/components/cart-page/components/order-form/components/product-cards-list/components/product-card/product-card.tsx
+++ src/components/cart-page/components/order-form/components/product-cards-list/components/product-card/product-card.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useState } from 'react';
 import { ProductInCart } from '../../../../../../../../type/product-in-cart';
 
 type ProductCardProps = {
@@ -13,22 +14,70 @@ function ProductCard({ product }: ProductCardProps) {
     amount
   } = product;
 
+  const [productCount, setProductCount] = useState(amount);
+
+  const handleInputCountChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    setProductCount(Number(target.value));
+  };
+
+  const handleInputCountBlur = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    if (Number(target.value) > 99) {
+      setProductCount(99);
+    } else if (Number(target.value) < 1) {
+      setProductCount(1);
+    } else {
+      setProductCount(Number(target.value));
+    }
+  };
+
+  const handleDecButtonClick = () => {
+    if (productCount > 1) {
+      setProductCount(productCount - 1);
+    }
+  };
+
+  const handleIncButtonClick = () => {
+    if (productCount < 99) {
+      setProductCount(productCount + 1);
+    }
+  };
+
+  const isValidPrice = () => {
+    if (productCount === 0) {
+      return price;
+    }
+
+    if (productCount > 99) {
+      return price * 99;
+    }
+
+    return price * productCount;
+  };
+
   return (
     <article className="product-card">
       <img className="product-card__img" src={img} alt="Товар" width="150" height="150" />
       <div className="product-card__content">
         <div className="product-card-title__wrap">
           <p className="product-card-title">{name}</p>
-          <p className="product-card-price">{price * amount} руб.</p>
+          <p className="product-card-price">
+            {isValidPrice()} руб.
+          </p>
         </div>
         <p className="product-card-description">{type} </p>
         <div className="product-card-counter__wrap">
           <div className="product-card-counter">
-            <button className="counter-button counter-button__minus" type='button'></button>
+            <button className="counter-button counter-button__minus" type='button' onClick={handleDecButtonClick}></button>
 
-            <input className="counter-input" type="number" defaultValue={amount} />
+            <input
+              className="counter-input"
+              type="number"
+              value={productCount}
+              onChange={handleInputCountChange}
+              onBlur={handleInputCountBlur}
+            />
 
-            <button className="counter-button counter-button__plus" type='button'></button>
+            <button className="counter-button counter-button__plus" type='button' onClick={handleIncButtonClick}></button>
           </div>
           <button className="product-card-delete" type='button'>Удалить</button>
         </div>
