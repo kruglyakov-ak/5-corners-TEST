e125b3e22265fadec10f9ce85fe6e04ab02d9fdb
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2022-03-02 01:32:23 +0300

    Исправил ошибки линтера

--- src/const.ts
+++ src/const.ts
@@ -1,10 +1,10 @@
-import { LatLngTuple } from "leaflet"
+import { LatLngTuple } from 'leaflet';
 
 export const TileLayerOptions = {
   Attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   Url: 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
   Id: 'mapbox/streets-v11',
   AccessToken: 'pk.eyJ1IjoiYWxla3NhbmRyLWthIiwiYSI6ImNrenp1ZDUxeDAwdGEzcG1zYmUwbmQ0eGgifQ.v5BxlNdNcGdmEbsehj41vQ'
-}
+};
 
-export const POSITION: LatLngTuple = [59.939099, 30.315877]
+export const POSITION: LatLngTuple = [59.939099, 30.315877];
